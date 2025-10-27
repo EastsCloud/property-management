877b9e5fdc799c1f12ebaf00a8ef11f7ffda2121
EastsCloud <95631880+EastsCloud@users.noreply.github.com>
2025-10-27 09:36:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request, redirect, url_for, flash
 from config import Config
 from models import db, Owner, ChargeType, Invoice, Payment, Equipment, MaintenancePlan, WorkOrder, Announcement
