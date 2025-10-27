846636da8a702dab1b048839aa961a96071e35f5
EastsCloud <95631880+EastsCloud@users.noreply.github.com>
2025-10-27 09:35:39 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
-
 import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///pm.db")
+    db_url = os.environ.get("DATABASE_URL", "sqlite:///pm.db")
+    
+    # Render
+    if db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+    
+    SQLALCHEMY_DATABASE_URI = db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
